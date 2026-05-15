d3b36f5bf7ba8bc1bc835cfdb875eeed91601d46
time2049 <58364786+time2049@users.noreply.github.com>
2026-05-15 09:12:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+#
+# Copyright (c) 2019-2026 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 添加 PassWall 源（开发版使用 main 分支）
+# 1. 添加 PassWall 源（使用 main 分支以适配最新内核）
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
 echo 'src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
 
-# 添加 OpenClash 源
+# 2. 添加 OpenClash 源（全能代理必备）
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
 
-# 添加 Argon 主题源
+# 3. 添加 Argon 主题及其配置插件
 echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
-
-# 添加 Argon 配置插件（开发版兼容）
 echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config.git' >> feeds.conf.default
+
+# 4. (可选) 如果你需要更多插件，可以在下方继续添加
